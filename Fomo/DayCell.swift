31a7d03853d0d86b696e0990e72488f112e0d12c
Christian Deonier <cdeonier@gmail.com>
2016-03-05 14:36:15 -0800

    Add day section headers

---
+++ Fomo/DayCell.swift
@@ -0,0 +1,24 @@
+//
+//  DayCell.swift
+//  Fomo
+//
+//  Created by Christian Deonier on 3/5/16.
+//  Copyright © 2016 TeamAwesome. All rights reserved.
+//
+
+import UIKit
+
+class DayCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
