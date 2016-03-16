791fd82958389df4e1a09934e4558848fe7b942c
Christian Deonier <cdeonier@gmail.com>
2016-03-15 23:56:39 -0700

    fix parameters

--- Fomo/RecommenderClient.swift
+++ Fomo/RecommenderClient.swift
@@ -110,10 +110,10 @@ class RecommenderClient: BDBOAuth1RequestOperationManager {
             "userEmail": itinerary.creator!.email!,
             "name": itinerary.creator!.name!,
             "tripName": itinerary.tripName!,
-            "startDate": DateFormatter.dateTostring(itinerary.startDate)!,
+            "startDate": DateFormatter.dateTostring(itinerary.startDate!)!,
             "numDays" : "\(itinerary.numDays!)",
             "location": location,
-            "radius": "\(itinerary.city?.radius!)" ]
+            "radius": "\((itinerary.city?.radius)!)" ]
         requestPOSTWithItineraryResponse(url, parameters: parameters, completion: completion)
         
     }
